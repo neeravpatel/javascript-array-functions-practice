5b5311116de9f5000dd12db843d75b261d6e14cc
Neerav Patel <nickpatel.developer@gmail.com>
2021-04-25 21:13:07 -0400

    Javascipt map function practice

--- worksheet.js
+++ worksheet.js
@@ -1,40 +1,50 @@
 const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: 172,
-        mass: 77,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: 202,
-        mass: 136,
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: 150,
-        mass: 49,
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: 188,
-        mass: 84,
-        eye_color: 'blue',
-        gender: 'male',
-    },
+  {
+    name: "Luke Skywalker",
+    height: 172,
+    mass: 77,
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: 202,
+    mass: 136,
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: 150,
+    mass: 49,
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: 188,
+    mass: 84,
+    eye_color: "blue",
+    gender: "male",
+  },
 ];
 
 //***MAP***
 //1. Get array of all names
+let names = characters.map((character) => character.name);
+console.log(names);
 //2. Get array of all heights
+let heights = characters.map((character) => character.height);
+console.log(heights);
 //3. Get array of objects with just name and height properties
+let obj = characters.map((character) => ({
+  name: character.name,
+  height: character.height,
+}));
+console.log(obj);
 //4. Get array of all first names
-
+let firstName = characters.map((character) => character.name.split(" ")[0]);
+console.log(firstName);
 //***REDUCE***
 //1. Get total mass of all characters
 //2. Get total height of all characters
